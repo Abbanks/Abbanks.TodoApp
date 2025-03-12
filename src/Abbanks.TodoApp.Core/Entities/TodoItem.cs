05e381596bc9de4f7f3d7c51aa06f98962c991ec
Oluwabanke Eboda <abeegarl@gmail.com>
2025-03-12 03:43:27 +0100

    fix ci/cd issue

--- src/Abbanks.TodoApp.Core/Entities/TodoItem.cs
+++ src/Abbanks.TodoApp.Core/Entities/TodoItem.cs
@@ -6,7 +6,7 @@ namespace Abbanks.TodoApp.Core.Entities
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; } 
+        public string Description { get; set; }
         public DateTime DueDate { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? CompletedAt { get; set; }
