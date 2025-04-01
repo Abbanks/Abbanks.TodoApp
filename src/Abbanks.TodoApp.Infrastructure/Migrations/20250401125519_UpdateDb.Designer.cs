ab5b8dc09d7b34dadbeb565f0f9b8deaad843259
Oluwabanke Eboda <abeegarl@gmail.com>
2025-04-01 14:06:18 +0100

    add new migration for user data and seeded data

--- src/Abbanks.TodoApp.Infrastructure/Migrations/20250401125519_UpdateDb.Designer.cs
+++ src/Abbanks.TodoApp.Infrastructure/Migrations/20250401125519_UpdateDb.Designer.cs
@@ -4,6 +4,7 @@ using Abbanks.TodoApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Abbanks.TodoApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250401125519_UpdateDb")]
+    partial class UpdateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
